8b103299bbb7e1f6bdecd79c452eaa038a009741
Nikhil Jain <blockerdude2@yahoo.com>
2016-03-15 02:34:15 -0500

    GoogleParser createCommonDescriptor
    First Iteration at a createCommonDescriptor method. Untested.

--- Guqu/Guqu/Models/GoogleDriveCommunicationParser.cs
+++ Guqu/Guqu/Models/GoogleDriveCommunicationParser.cs
@@ -4,61 +4,64 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Dynamic;
 
 namespace Guqu.Models
 {
     class GoogleDriveCommunicationParser : ICommunicationParser
     {
         
-        private static Dictionary<string, string> google_CD_Dictionary;
+        private static Dictionary<string, string> cd_google_Term_Dictionary;
+        private Dictionary<string, string> cd_google_Value_Dictionary;
         public GoogleDriveCommunicationParser()
         {
-
-            google_CD_Dictionary = new Dictionary<string, string>();
-            google_CD_Dictionary.Add("title", "fileName");
-            google_CD_Dictionary.Add("mimeType", "fileType");
-            google_CD_Dictionary.Add("fileSize", "fileSize");
-            google_CD_Dictionary.Add("modifiedDate", "lastModified");
-
-
-            //TODO: pass in the relative filepath? The overall controller should have it.
-            // commonDescriptor_google_Dictionary.Item["filePath"] = 
-
-
-            //           private string fileName, fileType, filePath;
-            // private DateTime lastModified;
-            //private int fileSize;
+            cd_google_Term_Dictionary = new Dictionary<string, string>();
+            cd_google_Term_Dictionary.Add("fileName", "title");
+            cd_google_Term_Dictionary.Add("fileType", "mimeType");
+            cd_google_Term_Dictionary.Add("fileSize", "fileSize");
+            cd_google_Term_Dictionary.Add("lastModified", "modifiedData");
         }
 
         //Required by interface
         public CommonDescriptor createCommonDescriptor(StreamReader fileStreamReader, string relativeFilePath)
         {
+            string googleJsonData = fileStreamReader.ReadToEnd();
             Dictionary<string, string> mdValues = new Dictionary<string, string>();
-            string curLine;
-            string curElement;
-            int firstIndex, secondIndex, beginValueIndex, endValueIndex;
-            while((curLine = fileStreamReader.ReadLine()) != null)
+            if (googleJsonData != null)
             {
-                //see if the line contains " "
-                if (curLine.Contains('"'))
+                JObject googleMD = JObject.Parse(googleJsonData);
+                JToken curToken;
+                string mdValue;
+                foreach(KeyValuePair<string, string> entry in cd_google_Term_Dictionary)
                 {
-                    firstIndex = curLine.IndexOf('"');
-                    secondIndex = curLine.IndexOf('"', firstIndex);
-                    //TODO: check for OBO
-                    curElement = curLine.Substring(firstIndex, secondIndex - firstIndex);
-
-                    //check to see if the element is required.
-                    if (google_CD_Dictionary.ContainsValue(curElement))
-                    {
-                        //write the value to mdValues
-                        beginValueIndex = curLine.IndexOf(':');
-                        //TODO, add the value to the md valued dict. key = value from google_CD_dictionary, value is substring from beginValueIndex.
-                        //mdValues.Add(google_CD_Dictionary.get)
-                    }
+                    //todo: should we use trygetvalue?
+                    curToken = googleMD.GetValue(entry.Value);
+                    //curToken holds the data from Jobject for a specific field.
+                    mdValue = (string)Convert.ChangeType(curToken.ToString(), typeof(string));
+                    //mdValue gets the value for that field 
+                    cd_google_Value_Dictionary.Add(entry.Key, mdValue);
+                    //cd_google_Value_Dictionary holds the pairing from cd terms to the actual values.
                 }
+                //have all of the needed information to create a CD object.
+
+                //TODO: is there a better way to do the following?
+                string fileName, fileType, fsize, lastMod;
+                DateTime lastModified;
+                int fileSize;
+                cd_google_Value_Dictionary.TryGetValue("fileName", out fileName);
+                cd_google_Value_Dictionary.TryGetValue("fileType", out fileType);
+                cd_google_Value_Dictionary.TryGetValue("fileSize", out fsize);
+                cd_google_Value_Dictionary.TryGetValue("lastModified", out lastMod);
+                lastModified = Convert.ToDateTime(lastMod);
+                Int32.TryParse(fsize, out fileSize);
+
+                CommonDescriptor cd = new CommonDescriptor(fileName, fileType, relativeFilePath, lastModified, fileSize);
+                return cd;
             }
-            //to create a common descriptor object we need to find everything in the hashmap. 
             return null;
+
+
         }
         //Required by interface
         public string createUploadBody(ServiceDescriptor descriptor)
