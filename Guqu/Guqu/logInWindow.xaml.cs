03d36c8dd783dd230625c2157751c7153bc1d2aa
Brent Grimm <bwgrimm@wisc.edu>
2016-03-15 15:18:43 -0500

    made in window error messages and fixed password box for login view

--- Guqu/Guqu/logInWindow.xaml.cs
+++ Guqu/Guqu/logInWindow.xaml.cs
@@ -26,15 +26,44 @@ namespace Guqu
 
         private void loginClick(object sender, RoutedEventArgs e)
         {
-            
+            if (usernameExists(textBox.Text.ToString()))
+            {
+                if (passwordCorrect(passwordBox.Password.ToString()))
+                {
+                    MainWindow mainWin = new MainWindow();
+                    mainWin.Show();
+                    this.Close();
+                }
+                else//passwordIncorect
+                {
+                    errorMessage.Text = "Incorrect password.";
+                }
+            }
+            else//user name doesn't exist
+            {
+                errorMessage.Text = "Username does not exist.";
+            }
+
+
         }
+        //check if username given exists
+        private bool usernameExists(String username)
+        {
+            return true;
+        }
+        //check to see if password for given username is correct
+        private bool passwordCorrect(String password)
+        {
+            return true;
+        }
+
         private void createAccountClick(object sender, RoutedEventArgs e)
         {
             //if (!acceptableEmailAddress())
             //{
             // error message  
             //}
-            //if (!acceptablePassord())
+            //if (!acceptablePassword())
             //{
             // error message
             //}
@@ -42,6 +71,5 @@ namespace Guqu
             createWin.Show();
             this.Close();
         }
-        
     }
 }
