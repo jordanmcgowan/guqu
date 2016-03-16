9f866bf2b61957974fa1557cc9b700f8009452e3
Nikhil Jain <blockerdude2@yahoo.com>
2016-03-15 21:56:11 -0500

    OneDriveCommunication Parser. Pass 2
    JsonFileParser added to make communicationParser easier to scale. Google
    and one drive changed to use jsonfileparser.

--- Guqu/Guqu/Models/GoogleDriveCommunicationParser.cs
+++ Guqu/Guqu/Models/GoogleDriveCommunicationParser.cs
@@ -23,69 +23,58 @@ namespace Guqu.Models
         //Required by interface
         public CommonDescriptor createCommonDescriptor(StreamReader fileStreamReader, string relativeFilePath)
         {
-            string googleJsonData = fileStreamReader.ReadToEnd();
-            Dictionary<string, string> mdValues = new Dictionary<string, string>();
-            if (googleJsonData != null)
+
+            JsonFileParser parser = new JsonFileParser();
+            cd_google_Value_Dictionary = parser.retrieveValues(cd_google_Term_Dictionary, fileStreamReader);
+
+            //variables to pass in
+            string fileName, fileType, fsize, lastMod;
+            DateTime lastModified;
+            int fileSize;
+
+            //google specific logic for creation
+            cd_google_Value_Dictionary.TryGetValue("fileName", out fileName);
+            cd_google_Value_Dictionary.TryGetValue("fileType", out fileType);
+            if (fileType.Equals("application/vnd.google-apps.folder")) //googles folders are called this.
             {
-                JObject googleMD = JObject.Parse(googleJsonData);
-                JToken curToken;
-                string mdValue;
-                foreach(KeyValuePair<string, string> entry in cd_google_Term_Dictionary)
-                {
-                    //todo: should we use trygetvalue?
-                    curToken = googleMD.GetValue(entry.Value);
-                    //curToken holds the data from Jobject for a specific field.
-                    mdValue = (string)Convert.ChangeType(curToken.ToString(), typeof(string));
-                    //mdValue gets the value for that field 
-                    cd_google_Value_Dictionary.Add(entry.Key, mdValue);
-                    //cd_google_Value_Dictionary holds the pairing from cd terms to the actual values.
-                }
-                //have all of the needed information to create a CD object.
-
-                //TODO: is there a better way to do the following?
-                string fileName, fileType, fsize, lastMod;
-                DateTime lastModified;
-                int fileSize;
-                cd_google_Value_Dictionary.TryGetValue("fileName", out fileName);
-                cd_google_Value_Dictionary.TryGetValue("fileType", out fileType);
-                cd_google_Value_Dictionary.TryGetValue("fileSize", out fsize);
-                cd_google_Value_Dictionary.TryGetValue("lastModified", out lastMod);
-                lastModified = Convert.ToDateTime(lastMod);
-                Int32.TryParse(fsize, out fileSize);
-
-                CommonDescriptor cd = new CommonDescriptor(fileName, fileType, relativeFilePath, lastModified, fileSize);
-                return cd;
+                //do translaion for fileType
+                fileType = "folder";
             }
-            return null;
+            cd_google_Value_Dictionary.TryGetValue("fileSize", out fsize);
+            cd_google_Value_Dictionary.TryGetValue("lastModified", out lastMod);
+            lastModified = Convert.ToDateTime(lastMod);
+            Int32.TryParse(fsize, out fileSize);
 
+            CommonDescriptor cd = new CommonDescriptor(fileName, fileType, relativeFilePath, lastModified, fileSize);
 
+            return cd;
         }
         //Required by interface
         public string createUploadBody(ServiceDescriptor descriptor)
         {
             StringBuilder builder = new StringBuilder("");
-            builder.Append("POST /upload/drive/v3/files?uploadType=multipart HTTP/1.1\n");
-            builder.Append("Host: www.googleapis.com\n");
+            builder.AppendLine("POST /upload/drive/v3/files?uploadType=multipart HTTP/1.1");
+            builder.AppendLine("Host: www.googleapis.com");
             builder.Append("Authorization: Bearer ");
             //TODO: ensure this is the correct file location of the keys.
             //TODO: put these files with the metaData?
             //TODO: create a 'keys' controller that can give the absolute path of a file for something like that.
             string google_auth_token = File.ReadAllText("..\\keys\\guqu_drive_client_id.json");
-            builder.Append(google_auth_token + "\n");
+            builder.AppendLine(google_auth_token);
 
             //TODO: use a unique string to act as the boundary calls for all uploads? For google, for everything?
             string boundary = "BOUNDARYSTRING";
-            builder.Append("Content-Type: multipart/related; boundary=" + boundary + "\n");
+            builder.AppendLine("Content-Type: multipart/related; boundary=" + boundary);
 
             //Dictionary<string, string> dict = descriptor.getRequestHeaders();
             CommonDescriptor cd = descriptor.getCommonDescriptor();
             
-            builder.Append("Content-Length: " + cd.FileSize + "\n\n");
-            builder.Append("--" + boundary);
-            builder.Append("Content-Type: application/json; charset=UTF-8\n\n");
-            builder.Append("{ name: " + cd.FileName + " }\n");
-            builder.Append("--" + boundary);
-            builder.Append("Content-Type: " + cd.FileType);
+            builder.AppendLine("Content-Length: " + cd.FileSize + "\n");
+            builder.AppendLine("--" + boundary);
+            builder.AppendLine("Content-Type: application/json; charset=UTF-8\n");
+            builder.AppendLine("{ name: " + cd.FileName + " }");
+            builder.AppendLine("--" + boundary);
+            builder.AppendLine("Content-Type: " + cd.FileType);
 
             //TODO: then you add the data to this, and terminate with another boundary string.
 
