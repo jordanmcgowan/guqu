a888b0ca32621214608b55f1190476164453b20a
Nikhil Jain <blockerdude2@yahoo.com>
2016-03-15 19:23:15 -0500

    FileHierarchy first pass
    MetaDataController can now iterate through the file directory and build
    a tree of treeNodes to eventually be passed to a view to populate info
    their.

--- Guqu/Guqu/Models/MetaDataController.cs
+++ Guqu/Guqu/Models/MetaDataController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.IO;
 using System.Web.Script.Serialization;
+using Guqu.Models.SupportClasses;
+using System.Collections.Generic;
+
 namespace Guqu.Models
 {
     class MetaDataController
@@ -27,6 +30,7 @@ namespace Guqu.Models
         */
         public CommonDescriptor getCommonDescriptorFile(string filePath)
         {
+            //TODO: find a way to differentiate between the CD for the folder and for a file named the same as the folder.
             JavaScriptSerializer jsonSerializer = new JavaScriptSerializer();
             string jsonCD = File.ReadAllText(rootStoragePath + COMMONDESCRIPTORPATH + filePath +".json");
             CommonDescriptor cd = jsonSerializer.Deserialize<CommonDescriptor>(jsonCD);
@@ -112,6 +116,55 @@ namespace Guqu.Models
                 File.Delete(cdPath);
             }
             return true;
+        }
+
+        public TreeNode getRoot(string account)
+        {
+
+            TreeNode root = new TreeNode(null, null);
+            string rootPath = rootStoragePath + METADATAPATH + account;
+            return createTree(root, rootPath);
+            
+        }
+        private TreeNode createTree(TreeNode rootNode, string rootFilePath)
+        {
+            var folderQueue = new Queue<TreeNode>();
+            //get everything in this directory. Should be only folders and CD.json files
+            string[] subDirs = Directory.GetFiles(rootFilePath, "*.json", SearchOption.TopDirectoryOnly);
+            FileAttributes curAttr;
+            CommonDescriptor curfileCD;
+            TreeNode childNode;
+
+            foreach(string file in subDirs)
+            {
+                curAttr = File.GetAttributes(file);
+                //is a CD.json file 
+                if (!curAttr.HasFlag(FileAttributes.Directory))
+                {
+                    //add as children
+                    curfileCD = getCommonDescriptorFile(file);
+                    childNode = new TreeNode(rootNode, curfileCD);
+                   
+                    //TODO: ensure that all CD store fiile type for folders as 'folder'
+                    //Because each actual file is terminated with _file.json, we can be sure
+                    //that the directory (which is not terminated) can be found by removing .json from the string
+                    if(curfileCD.FileType.Equals("folder"))
+                    {
+                        //only will recurse upon directories
+                        rootNode.addChild(createTree(rootNode, file.Replace(".json", ""));                      
+                    }
+                    else
+                    {
+                        //only add the normal files to the rootNode now.
+                        rootNode.addChild(childNode);
+                    }
+
+                }
+
+            }
+
+            return rootNode;
+
         }
 
     }
---
+++ Guqu/Guqu/Models/SupportClasses/TreeNode.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guqu.Models.SupportClasses
+{
+    class TreeNode
+    {
+        private CommonDescriptor commonDescriptor;
+        private TreeNode parent;
+        private LinkedList<TreeNode> children;
+
+        public TreeNode(TreeNode parent, CommonDescriptor cd)
+        {
+            this.parent = parent;
+            commonDescriptor = cd;
+            children = new LinkedList<TreeNode>();
+            children.Clear();
+        }
+        public TreeNode getParent()
+        {
+            return parent;
+        }
+        public LinkedList<TreeNode> getChildren()
+        {
+            return children;
+        }
+        public void addChild(TreeNode child)
+        {
+            children.AddLast(child);
+        }
+    }
+}
