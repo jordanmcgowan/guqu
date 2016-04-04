e97ee69ef57fe33ab05efbaf42fc8908530033e8
Brent Grimm <bwgrimm@wisc.edu>
2016-04-03 20:12:01 -0500

    Combined menus

--- Guqu/Guqu/WebServices/InitializeAPI.cs
+++ Guqu/Guqu/WebServices/InitializeAPI.cs
@@ -10,14 +10,9 @@ using Google.Apis.Drive.v3;
 using Google.Apis.Drive.v3.Data;
 using Google.Apis.Services;
 using Google.Apis.Util.Store;
-using System;
-using System.Collections.Generic;
+
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
-using System.Net.Http;
 
 //Box Usages
 using Box.V2;
