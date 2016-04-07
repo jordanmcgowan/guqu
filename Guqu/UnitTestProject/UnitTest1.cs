f3a978ecb59f3609946fa7bac0534c4ea5e4b822
brentgrim <brentisgrimm@gmail.com>
2016-04-07 00:19:32 -0500

    Simplest test possible

---
+++ Guqu/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Guqu;
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            logInWindow winUT = new logInWindow();
+            Assert.AreEqual(300, winUT.Height);
+            Assert.AreEqual(300, winUT.Width);
+
+        }
+    }
+}
