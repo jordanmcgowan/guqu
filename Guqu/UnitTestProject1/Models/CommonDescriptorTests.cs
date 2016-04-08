a116d7c5086ab652b464ac1eca275d49c7a73a81
mwilcox-wi <mwilcox.wi@gmail.com>
2016-04-07 21:05:59 -0500

    2 lost files added

---
+++ Guqu/UnitTestProject1/Models/CommonDescriptorTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Guqu.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guqu.Models.Tests
+{
+    [TestClass()]
+    public class CommonDescriptorTests
+    {
+        [TestMethod()]
+        public void CommonDescriptorTest()
+        {
+            string name, type, path, id;
+            name = "name";
+            type = "type";
+            path = "path";
+            DateTime date = DateTime.Now;
+            long size = 1024; ;
+            id = "id";
+
+            CommonDescriptor con = new CommonDescriptor(name, type, path, id, date, size);
+            CommonDescriptor cd = con;
+            cd.FileName = name;
+            cd.FileType = type;
+            cd.FilePath = path;
+            cd.LastModified = date;
+            cd.FileSize = size;
+            cd.FileID = id;
+
+            Assert.AreEqual(cd, con);
+            
+        }
+
+        
+        
+    }
+}
---
+++ Guqu/UnitTestProject1/Models/GoogleDriveCommunicationParserTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Guqu.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guqu.Models.Tests
+{
+    [TestClass()]
+    public class GoogleDriveCommunicationParserTests
+    {
+        
+        [TestMethod()]
+        public void createCommonDescriptorTest()
+        {
+            GoogleDriveCommunicationParser gdcp = new GoogleDriveCommunicationParser();
+            string json = "{ \"FileName\":\"Delt Calendar 2015\",\"FileType\":\"application/vnd.google-apps.spreadsheet\",\"FilePath\":\"GoogleDrive\\Delta Tau Delta - Beta Gamma\\The Delt Library\",\"FileID\":\"1Sjaiv_xT_wvuoSvjy7lQeU09QY6kQ6DLuSRciYvx9ys\",\"LastModified\":\"/Date(1449039649000)\",\"FileSize\":0}";
+        string relPath = "GoogleDrive\\Delta Tau Delta - Beta Gamma\\The Delt Library";
+
+            var cd = gdcp.createCommonDescriptor(relPath, json);
+
+            if (cd == null)
+                Assert.Fail();
+
+
+            
+
+        }
+
+        [TestMethod()]
+        public void convertExtensionTest()
+        {
+            var gdcp = new GoogleDriveCommunicationParser();
+            string oldEx, newEx;            
+            oldEx = "application / vnd.google - apps.document";
+            newEx = gdcp.convertExtension(oldEx);
+
+            if (newEx != ".doc")
+                Assert.Fail();
+
+
+
+        }
+
+        [TestMethod()]
+        public void getMimeTypeTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
