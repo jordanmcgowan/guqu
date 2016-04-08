940e0baee39687eef5fe06d261fc1de91b9012f5
jordanmcgowan <j.m.0604>
2016-04-07 21:19:00 -0500

    Updates incorrect password behavior to show error message. Also adds comments and cleans up code

--- Guqu/Guqu/Views/logInWindow.xaml.cs
+++ Guqu/Guqu/Views/logInWindow.xaml.cs
@@ -71,16 +71,17 @@ namespace Guqu
                     using (var deriveBytes = new Rfc2898DeriveBytes(pass, salt))
                     {
                         byte[] newKey = (deriveBytes.GetBytes(20));  // derive a 20-byte key
-                        //Checks to see if keys are the same
-                        //Console.WriteLine(Convert.ToBase64String(newKey) + " ----- " + Convert.ToBase64String(key));
-
+                        //Checks to see if keys are NOT the same
                         if (!newKey.SequenceEqual(key))
-                            Console.WriteLine("Password is invalid!");
+                            errorMessage.Text = "Incorrect Password.";
+                        //If keys are the same
                         else
                         {
                             List<UserCloud> userClouds;
+                            //Ensure user has user clouds
                             if (db.doesUserCloudExist(user.User_id))
                             {
+                                //Gather and display user clouds
                                 userClouds = db.SelectUserClouds(user.User_id);
                                 foreach (UserCloud cloud in userClouds)
                                 {
@@ -98,93 +99,21 @@ namespace Guqu
 
                                 }
                             }
-
                             MainWindow mainWin = new MainWindow(user);
                             mainWin.Show();
                             this.Close();
                         }
                     }
-                    /*
-                    if (user.Pass_hash == pass) //TODO: Iteration 2: add hasing & salting
-                    {
-                        MainWindow mainWin = new MainWindow(user);
-                        mainWin.Show();
-                        this.Close();
-                    }
-                    
-                    else
-                    {
-                        errorMessage.Text = "Incorrect Password.";
-                    }
-                    */
                 }
                 else
                 {
                     errorMessage.Text = "Username does not exist.";
                 }
-
-
-                /*
-                if (usernameExists(email))
-                {    
-                    
-                    MainWindow mainWin = new MainWindow();
-                    mainWin.Show();
-                    this.Close();
-
-                }
-                else//user name doesn't exist
-                {
-                    errorMessage.Text = "Username does not exist.";
-                }
-                */
             }
         }
-
-        /*
-        //check if username given exists
-        private bool usernameExists(string email)
-        {
-            //Query DB users table for username entered
-            ServerCommunicationController db = new ServerCommunicationController(); //TODO: make this object as global
-            List<String> list = db.Select("users", email);
-            //Username exists in users table
-            if (list.Count > 0){
-                if (list[1] == email)
-                {
-                    //Ensures password is correct for given user 
-                    passwordCorrect(list[4], pass);
-                    return true;
-                }
-                //No entry in DB for that username
-                else{
-                    return false;
-                }
-            }
-            //No entry in DB for that username
-            else{
-                return false;
-            }
-        }
-      
-        //check to see if password for given username is correct
-        private bool passwordCorrect(string dbPassword, string enteredPassword)
-        {
-            if (enteredPassword == dbPassword)
-            {
-                return true;
-            }
-            else
-            {
-                errorMessage.Text = "Incorrect password.";
-                return false;
-            }
-        }
-        */
 
         private void createAccountClick(object sender, RoutedEventArgs e)
         {
-
             createAccountWindow createWin = new createAccountWindow();
             createWin.Show();
             this.Close();
